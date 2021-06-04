1de03849690b20d8b6dadc89d907e7f576b24b38
Dustin Yost <dustin.yost.t@gmail.com>
2021-06-04 19:47:44 -0400

    Replace note about phyx with rapier

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -13,8 +13,8 @@
 //! Library Notes:
 //! - [libloading](https://docs.rs/libloading/0.7.0/libloading/) for plugin loading/execution. [See guide for more.](https://michael-f-bryan.github.io/rust-ffi-guide/dynamic_loading.html)
 //! - [async asset loading](https://rust-lang.github.io/async-book/01_getting_started/02_why_async.html)
-//! - [laminar](https://crates.io/crates/laminar) as a replacement for Game Networking Sockets
-//! - [physx](https://crates.io/crates/physx)
+//! - [networking - laminar](https://crates.io/crates/laminar) as a replacement for Game Networking Sockets
+//! - [physics - rapier](https://crates.io/crates/rapier3d)
 //! - [profiling](https://crates.io/crates/profiling)
 //! - [cryptography](https://crates.io/crates/rustls)
 //! - [noise](https://crates.io/crates/noise) for randomization and noise in chunk generation
