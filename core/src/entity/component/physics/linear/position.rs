14a614388c36371912b88886aaa98915440efbc8
temportalflux <dustin.yost.t@gmail.com>
2022-03-06 15:35:02 -0500

    Bump engine

--- core/src/entity/component/physics/linear/position.rs
+++ core/src/entity/component/physics/linear/position.rs
@@ -105,7 +105,7 @@ impl std::ops::AddAssign<Vector3<f32>> for Position {
 }
 
 impl network::Replicatable for Position {
-	fn on_replication(&mut self, replicated: &Self, is_locally_owned: bool) {
+	fn on_replication(&mut self, replicated: &Self, _is_locally_owned: bool) {
 		/*
 		if is_locally_owned {
 			let offset =
